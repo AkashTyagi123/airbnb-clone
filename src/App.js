9447b941fdc4c998b590e17305c29516a635c925
Akash Tyagi <tyagiakash630@gmail.com>
2020-10-19 19:41:48 +0530

    final product

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Search from './components/Home/Search/Search';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+      <Header />
+      <Switch>
+        <Route path="/search">
+          <SearchPage></SearchPage>
+        </Route>
+        <Route path="/" >
+        <Home/> 
+        </Route>
+
+      </Switch>
+         
+      <Footer/>
+      </Router>
+        {/* Header*/}
+        {/* Banner*/}
+         {/* Cards*/}
+         {/* Footer*/}
     </div>
   );
 }
